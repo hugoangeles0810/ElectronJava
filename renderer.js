7db48606d494bd0ae037e1d15d75f9d0cc96d0e3
Hugo Angeles <hugoangeles0810@gmail.com>
2016-06-08 02:11:51 -0500

    Invocando jar desde electron (web page con javascript)

--- renderer.js
+++ renderer.js
@@ -1,3 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+
+// For develop
+var path = require("path")
+JAR_PATH = path.join(process.cwd(), "ElectronUtil.jar");
+
+// For production
+/*JAR_PATH = path.join(process.cwd(), "resources", "app", "ElectronUtil.jar");*/
